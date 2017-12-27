911c2ec17cb0cee483a745615a130f06f384bd6a
git-oaktree <git-oaktree@users.noreply.github.com>
2017-12-27 12:31:00 -0800

    Update Get-BasicEnum.ps1

--- Get-BasicEnum.ps1
+++ Get-BasicEnum.ps1
@@ -17,17 +17,32 @@ function Get-BasicEnum{
         }
     }
 
+    write-output "##############################################################################"
     $noPerms | foreach-object { write-output "Permission denied for paths $_"}
-
-    gci 'c:\'
-
-    gci 'C:\Program Files' 
-
-    gci 'C:\Program Files (x86)'
-
-    get-process | select ProcessName, Path 
+       
+    write-output "##############################################################################"
+    write-output 'Directory listing of C:\'
+	gci 'c:\'
     
-    Get-WmiObject win32_service | select Name, DisplayName, @{Name="Path"; Expression={$_.PathName.split('"')[1]}} | Format-List
+    write-output "##############################################################################"
+	write-output 'Directory listing of C:\'
+	gci 'C:\Program Files' 
+    
+    write-output "##############################################################################"
+	write-output 'Directory listing of C:\Program Files (x86)'
+	gci 'C:\Program Files (x86)'
+    
+    write-output "##############################################################################"
+    write-output 'Process List'
+	get-process | select ProcessName, Path 
+    
+    write-output "##############################################################################"
+	write-output 'Services'
+	Get-WmiObject win32_service | select Name, DisplayName, @{Name="Path"; Expression={$_.PathName.split('"')[1]}} | Format-List
 
+	write-output "##############################################################################"
+	write-output 'Non-Standard Services'
     Get-wmiobject win32_service | where { $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows" -and $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" }
+
+
 }
