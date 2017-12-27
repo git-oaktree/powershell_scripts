845ea7bd1bccd16fafcaa42c1ed5ae9d04af8833
git-oaktree <git-oaktree@users.noreply.github.com>
2017-12-27 12:20:49 -0800

    Update Get-BasicEnum.ps1

--- Get-BasicEnum.ps1
+++ Get-BasicEnum.ps1
@@ -26,4 +26,8 @@ function Get-BasicEnum{
     gci 'C:\Program Files (x86)'
 
     get-process | select ProcessName, Path 
+    
+    Get-WmiObject win32_service | select Name, DisplayName, @{Name="Path"; Expression={$_.PathName.split('"')[1]}} | Format-List
+
+    Get-wmiobject win32_service | where { $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows" -and $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" }
 }
