d973b2c951439e06aef4dd047fbf9f7b25c74af6
git-oaktree <git-oaktree@users.noreply.github.com>
2017-12-27 14:14:44 -0800

    Update Get-BasicEnum.ps1

--- Get-BasicEnum.ps1
+++ Get-BasicEnum.ps1
@@ -25,7 +25,7 @@ function Get-BasicEnum{
 	gci 'c:\'
     
     write-output "##############################################################################"
-	write-output 'Directory listing of C:\program files'
+	write-output 'Directory listing of C:\'
 	gci 'C:\Program Files' 
     
     write-output "##############################################################################"
@@ -44,5 +44,11 @@ function Get-BasicEnum{
 	write-output 'Non-Standard Services'
     Get-wmiobject win32_service | where { $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows" -and $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" }
 
+	write-output "##############################################################################"
+	write-output 'GPO Results'
+    gporesults.exe /Z
 
+	write-output "##############################################################################"
+	write-output 'netstat output'
+    netstat -anp tcp 
 }
