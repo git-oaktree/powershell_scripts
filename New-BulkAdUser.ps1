1cd412871697242e62d5f7796a76e349830a8c6d
git-oaktree <git-oaktree@users.noreply.github.com>
2019-08-05 14:04:57 -0400

    Update New-BulkAdUser.ps1

--- New-BulkAdUser.ps1
+++ New-BulkAdUser.ps1
@@ -1,20 +1,20 @@
-$Users = Import-Csv  -Path "C:\Users\oaktree\Desktop\AD_User_creation.csv"            
+$Users = Import-Csv  -Path "C:\Users\Administrator\Desktop\bulk_user_import.csv"   
 foreach ($User in $Users)            
 {            
     #DC
-    $Displayname = $User.Firstname + " " + $User.Lastname            
-    $UserFirstname = $User.Firstname            
-    $UserLastname = $User.Lastname            
-    $OU = $User.OU           
+    $Displayname = $User.GivenName + " " + $User.SurName            
+    $UserFirstname = $User.GivenName            
+    $UserLastname = $User.SurName            
+    $OU = $User.Path           
     $SAM = $User.SAM            
-    $UPN = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain            
+    $UPN = $User.GivenName + "." + $User.SurName + "@" + "spiderweb.local"            
     $Description = $User.Description            
     $Password = $User.Password   
-    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false -PasswordNeverExpires $true 
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "CN=Users,DC=Spiderweb,DC=Local" -ChangePasswordAtLogon $false -PasswordNeverExpires $true 
 }          
 
 """
 sample of contents from within AD_User_creation.csv file. 
-Firstname	Lastname	Maildomain	SAM	OU	Password	Description
-Gregg	quintana	contoso.local	gquintana	OU=site DC,OU=Offices, DC=CONTOSO,DC=LOCAL	P@ssword	<description here> 
+Name,GivenName,SurName,UserPrincipalName,SamAccountName,Description,Department,EmployeeID,Path,Enabled,Password,PasswordNeverExpires
+Aaron Rodgers,Aaron,Rodgers,arodgers@contoso.local,arodgers,,,97368,"CN=Users,DC=Contoso,dc=local",$True,Trust123!,$True
 """
